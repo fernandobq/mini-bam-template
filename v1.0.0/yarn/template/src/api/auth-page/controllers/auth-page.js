4bedd465b572d70293cd3385a7e5c88129c592df
Fernando Barraza <fernandobarraza@192.168.1.9>
2023-02-01 11:44:25 -0700

    changed approach

--- v1.0.0/yarn/template/src/api/auth-page/controllers/auth-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * auth-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::auth-page.auth-page');
--- v1.0.0/yarn/template/src/api/auth-page/services/auth-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * auth-page service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::auth-page.auth-page');
